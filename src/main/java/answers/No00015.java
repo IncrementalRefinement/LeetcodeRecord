12aebd03610ddf12de9b89dac6bb20822403b6da
LTakamori <925951065@qq.com>
2021-08-22 12:36:08 +0800

    finished template

--- src/main/java/answers/No00015.java
+++ src/main/java/answers/No00015.java
@@ -31,26 +31,63 @@ public class No00015 {
 //        return res;
 //    }
 
-    // TODO: finish framework
     public static List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res = new LinkedList<>();
+        final int n = 3;
         Arrays.sort(nums);
-        nSum(3, nums, 0, 0, res);
-        return res;
+        return nSum(n, nums, 0, 0);
     }
 
-    private static void nSum(int n, int[] nums, int target, int beginIndex, List<List<Integer>> res) {
-        if (n == 2) {
-            twoSum(nums, target, beginIndex, res);
-        } else {
-            for (int i = beginIndex; i < nums.length; i++) {
-                nSum(n - 1, nums, target - nums[i], i + 1, res);
+    /*
+    precondition: nums 升序排列
+    返回 nums 中从 beginIndex 开始的子数组中的和为 target 的所有不重复的 n 个数的集合
+     */
+    private static List<List<Integer>> nSum(int n, int[] nums, int target, int beginIndex){
+        // TODO: n == 1?
+        List<List<Integer>> tempRes, currentRes = new LinkedList<>();
+        for (int i = beginIndex; i < nums.length - n + 1;) {
+            if (n == 3) {
+                tempRes = twoSum(nums, target - nums[i], i + 1);
+            } else {
+                tempRes = nSum(n - 1, nums, target - nums[i], i + 1);
+            }
+
+            if (tempRes.size() != 0) {
+                for (List<Integer> list : tempRes) {
+                    list.add(nums[i]);
+                }
+                currentRes.addAll(tempRes);
+                i++;
+                while (i < nums.length - n + 1 && nums[i] == nums[i - 1]) {
+                    i++;
+                }
+            } else {
+                // TODO: 多走几步？
+                i++;
             }
         }
+        return currentRes;
     }
 
-    private static void twoSum(int[] nums, int target, int beginIndex, List<List<Integer>> res) {
-
+    private static List<List<Integer>> twoSum(int[] nums, int target, int beginIndex) {
+        List<List<Integer>> res = new LinkedList<>();
+        int right = nums.length - 1;
+        for (int left = beginIndex; left < nums.length - 1 && left < right;) {
+            if (nums[left] + nums[right] == target) {
+                LinkedList<Integer> temp = new LinkedList<>();
+                temp.add(nums[left]);
+                temp.add(nums[right]);
+                res.add(temp);
+                left++; right--;
+                while (left < nums.length - 1 && nums[left] == nums[left - 1]) {
+                    left++;
+                }
+            } else if (nums[left] + nums[right] > target) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return res;
     }
 
     public static void main(String[] args) {

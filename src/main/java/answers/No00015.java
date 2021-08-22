645e4ba3124b8a2680df0e2a9a7033d0cba7ba33
LTakamori <925951065@qq.com>
2021-08-22 11:15:20 +0800

    passed leetcode

--- src/main/java/answers/No00015.java
+++ src/main/java/answers/No00015.java
@@ -1,11 +1,37 @@
 package answers;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
 public class No00015 {
-    // TODO: complete framework
+//    public static List<List<Integer>> threeSum(int[] nums) {
+//        final int target = 0;
+//        List<List<Integer>> res = new LinkedList<>();
+//        Arrays.sort(nums);
+//        for (int i = 0; i < nums.length - 2; i++) {
+//            if (i > 0 && nums[i] == nums[i - 1]) continue;
+//            int currentSum = nums[i];
+//            int right = nums.length - 1;
+//            for (int left = i + 1; left < right;) {
+//                if (currentSum + nums[left] + nums[right] == target) {
+//                    ArrayList<Integer> temp = new ArrayList<>(Arrays.asList(nums[i], nums[left], nums[right]));
+//                    res.add(temp);
+//                    left++;
+//                    right--;
+//                    while(left < right && nums[left] == nums[left - 1]) left++;
+//                } else if (currentSum + nums[left] + nums[right] < target) {
+//                    left++;
+//                } else {
+//                    right--;
+//                }
+//            }
+//        }
+//        return res;
+//    }
+
+    // TODO: finish framework
     public static List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> res = new LinkedList<>();
         Arrays.sort(nums);
@@ -25,5 +51,11 @@ public class No00015 {
 
     private static void twoSum(int[] nums, int target, int beginIndex, List<List<Integer>> res) {
 
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[] {-1,0,1,2,-1,-4};
+        var res = threeSum(nums);
+        System.out.println(res.size());
     }
 }

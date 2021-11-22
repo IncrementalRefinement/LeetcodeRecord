e87d0b8499e92c47270b6df72ef6133585ceee0a
LTakamori <fluent_python@qq.com>
2021-11-22 16:59:15 +0800

    fix

--- src/main/java/catagory/twopointer/No00080.java
+++ src/main/java/catagory/twopointer/No00080.java
@@ -2,8 +2,25 @@ package catagory.twopointer;
 
 public class No00080 {
     public static int removeDuplicates(int[] nums) {
-        int l = 0, r = 0;
-        // TODO
-        return 0;
+        int len = nums.length;
+        int l = 0, r = 0, current = 0;
+        while (r < len) {
+            while (r < len && nums[r] == nums[l]) {
+                r++;
+            }
+            if (r - l > 2) {
+                nums[current] = nums[l];
+                current++;
+                nums[current] = nums[l];
+                current++;
+            } else {
+                for (int i = 0; i < r - l; i++) {
+                    nums[current] = nums[l];
+                    current++;
+                }
+            }
+            l = r;
+        }
+        return current;
     }
 }

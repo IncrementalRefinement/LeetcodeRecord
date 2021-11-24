361a0d5bae170bd513d0d0ffdad65d3e281c5065
LTakamori <fluent_python@qq.com>
2021-11-24 16:51:24 +0800

    finish No42

---
+++ src/main/java/catagory/twopointer/No00142.java
@@ -0,0 +1,34 @@
+package catagory.twopointer;
+
+import util.ListNode;
+
+public class No00142 {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null) return null;
+
+        ListNode fast = head, slow = head;
+        boolean isCyclic = false;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                isCyclic = true;
+                break;
+            }
+        }
+
+        if (!isCyclic) {
+            return null;
+        }
+
+        fast = head;
+
+        while (fast != slow) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        return slow;
+    }
+}

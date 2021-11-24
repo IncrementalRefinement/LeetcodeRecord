0187e3079508fbfa8597145e7d65c518973a8da2
LTakamori <fluent_python@qq.com>
2021-11-24 17:26:49 +0800

    finish 00532

---
+++ src/main/java/catagory/twopointer/slidingwindow/No00532.java
@@ -0,0 +1,28 @@
+package catagory.twopointer.slidingwindow;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class No00532 {
+    public static int findPairs(int[] nums, int k) {
+        Map<Integer, Integer> theMap = new HashMap<>();
+        for (int num : nums) {
+            theMap.put(num, theMap.getOrDefault(num, 0) + 1);
+        }
+
+        int ret = 0;
+        for (int item : theMap.keySet()) {
+            if (k == 0) {
+                if (theMap.get(item) > 1) {
+                    ret++;
+                }
+            } else {
+                if (theMap.get(item + k) != null) {
+                    ret++;
+                }
+            }
+        }
+
+        return ret;
+    }
+}

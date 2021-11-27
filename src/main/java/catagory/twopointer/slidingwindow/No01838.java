7da8ec6d044c2b71dc873f0b12501ae921a5d395
LTakamori <fluent_python@qq.com>
2021-11-27 13:01:38 +0800

    Buenos Tardes

---
+++ src/main/java/catagory/twopointer/slidingwindow/No01838.java
@@ -0,0 +1,37 @@
+package catagory.twopointer.slidingwindow;
+
+import java.util.Arrays;
+
+// TODO: fix this shit
+public class No01838 {
+    public static int maxFrequency(int[] nums, int k) {
+        Arrays.sort(nums);
+        int l = 0, r = 0;
+        int current = nums[r];
+        int currentSum = 0;
+        int ret = 0;
+
+        while (r < nums.length) {
+            currentSum += (r - l) * (nums[r] - current);
+            current = nums[r];
+            r++;
+            if (currentSum <= k) {
+                if (r - l > ret) {
+                    ret = r - l;
+                }
+            } else {
+                while (currentSum > k) {
+                    currentSum -= (current - nums[l]);
+                    l++;
+                }
+            }
+        }
+
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        int res1 = maxFrequency(new int[]{1,2,4}, 5);
+        System.out.println(res1);
+    }
+}

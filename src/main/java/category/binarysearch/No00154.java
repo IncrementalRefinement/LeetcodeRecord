bc299248d5b0c16667378a0d1e78775a0c2674bc
LTakamori <fluent_python@qq.com>
2021-11-29 21:02:12 +0800

    finish 154

---
+++ src/main/java/category/binarysearch/No00154.java
@@ -0,0 +1,20 @@
+package category.binarysearch;
+
+public class No00154 {
+    public static int findMin(int[] nums) {
+        int l = 0, r = nums.length - 1;
+
+        while (l < r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] > nums[r]) {
+                l = mid + 1;
+            } else if (nums[mid] < nums[r]) {
+                r = mid;
+            } else {
+                r--;
+            }
+        }
+
+        return nums[l];
+    }
+}
